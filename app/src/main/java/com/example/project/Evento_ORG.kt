e857dde7450dc854d10542355fa4df093d60c44b
DiegoMTZ923 <A01634562@tec.mx>
2022-09-19 17:08:14 -0500

    eventos

---
+++ app/src/main/java/com/example/project/Evento_ORG.kt
@@ -0,0 +1,11 @@
+package com.example.project
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Evento_ORG : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_evento_org)
+    }
+}
