ed9953193efcc706483b89be124c34bf8e3d9ed7
Luis Angel <pirules_neta@yahoo.com.mx>
2022-09-19 17:49:50 -0500

    Super Borrador de activida organizacion

---
+++ app/src/main/java/com/example/project/Activity_Organizacion.kt
@@ -0,0 +1,11 @@
+package com.example.project
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Activity_Organizacion : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_organizacion)
+    }
+}
