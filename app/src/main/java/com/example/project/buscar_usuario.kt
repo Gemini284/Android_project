265523286878f6a22d8ce1eef94142a77a3fd54a
kaarlaglez <83715502+kaarlaglez@users.noreply.github.com>
2022-09-20 17:50:23 -0500

    buscar usuario

---
+++ app/src/main/java/com/example/project/buscar_usuario.kt
@@ -0,0 +1,11 @@
+package com.example.project
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class buscar_usuario : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_buscar_usuario)
+    }
+}
