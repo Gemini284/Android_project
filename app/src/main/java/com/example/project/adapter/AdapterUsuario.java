abddaed0b45161e6c19edbfc82e560c5d72b691e
kaarlaglez <83715502+kaarlaglez@users.noreply.github.com>
2022-10-07 13:44:40 -0500

    Buscar usuarios

---
+++ app/src/main/java/com/example/project/adapter/AdapterUsuario.java
@@ -0,0 +1,58 @@
+package com.example.project.adapter;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.project.R;
+import com.example.project.bd.Usuario;
+
+import java.util.List;
+
+public class AdapterUsuario extends RecyclerView.Adapter<AdapterUsuario.viewholderusuarios> {
+
+    List<Usuario> usuarioList;
+
+    public AdapterUsuario(List<Usuario> usuarioList) {
+        this.usuarioList = usuarioList;
+    }
+
+    @NonNull
+    @Override
+    public viewholderusuarios onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.row_usuarios,parent,false);
+        viewholderusuarios holder = new viewholderusuarios(v);
+
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull viewholderusuarios holder, int position) {
+
+        Usuario ms = usuarioList.get(position);
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return usuarioList.size();
+    }
+
+    public class viewholderusuarios extends RecyclerView.ViewHolder {
+
+        TextView tv_email, tv_nombre;
+
+
+        public viewholderusuarios(@NonNull View itemView) {
+            super(itemView);
+
+            tv_email = itemView.findViewById(R.id.tv_email);
+            tv_nombre = itemView.findViewById(R.id.tv_nombre);
+        }
+    }
+}
---
+++ app/src/main/java/com/example/project/bd/Usuario.java
@@ -0,0 +1,30 @@
+package com.example.project.bd;
+
+public class Usuario {
+    private String email;
+    private String nombre;
+
+    public Usuario() {
+    }
+
+    public Usuario(String email, String nombre) {
+        this.email = email;
+        this.nombre = nombre;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+}
