1197b887897dbe0bc1c9aa416d7339d30a504e7c
Mafer <A01634135@tec.mx>
2022-10-10 16:19:14 -0500

    Agregar constraints

--- app/src/main/java/com/example/project/MapsFragment.kt
+++ app/src/main/java/com/example/project/MapsFragment.kt
@@ -1,11 +1,15 @@
 package com.example.project
 
+import android.location.Location
 import androidx.fragment.app.Fragment
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationAvailability
+import com.google.android.gms.location.LocationServices
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -31,12 +35,14 @@ class MapsFragment : Fragment() {
         googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
     }
 
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_maps, container, false)
+
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -44,4 +50,6 @@ class MapsFragment : Fragment() {
         val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
         mapFragment?.getMapAsync(callback)
     }
+
+
 }
--- app/src/main/java/com/example/project/PuntoRecoleccionActivity.kt
+++ app/src/main/java/com/example/project/PuntoRecoleccionActivity.kt
@@ -1,28 +1,28 @@
 package com.example.project
 
 import android.Manifest
+import android.content.Context
 import android.content.pm.PackageManager
-import android.location.Location
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
-import com.google.android.gms.location.FusedLocationProviderClient
-import com.google.android.gms.location.LocationServices
-import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.MapFragment
+import com.google.android.material.internal.ContextUtils
+
 
 class PuntoRecoleccionActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_punto_recoleccion)
-        if(savedInstanceState == null){
+
+        //Añadir fragmento
+        if (savedInstanceState == null) {
             val fragment = MapsFragment()
             supportFragmentManager.beginTransaction()
                 .add(R.id.map_placeholder, fragment)
                 .commitAllowingStateLoss()
         }
-
     }
 
 }
